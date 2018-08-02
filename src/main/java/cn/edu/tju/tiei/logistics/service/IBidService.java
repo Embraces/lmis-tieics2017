ea8a8a350acf735f024f0ecabc657ffceb234a55
Embraces <37837097+Embraces@users.noreply.github.com>
2018-08-02 09:45:46 +0800

    Add files via upload

---
+++ src/main/java/cn/edu/tju/tiei/logistics/service/IBidService.java
@@ -0,0 +1,29 @@
+package cn.edu.tju.tiei.logistics.service;
+
+import java.util.List;
+
+import cn.edu.tju.tiei.logistics.model.Bid;
+
+
+
+public interface IBidService {
+	  /**
+     * Load all bids
+     * @return
+     */
+
+	    List<Bid> findAll();
+
+	    Bid findById(Integer id);
+	    //Bid get
+
+		void create(Bid bid);
+
+		boolean isExist(Bid bid);
+
+		void update(Bid bid);
+
+		void deleteById(Integer id);
+
+		void deleteAll();
+}
